53f2c0df3f77a51e8593eb9c7afe960c3a41a038
Muggle Wei <mugglewei@gmail.com>
2025-09-12 16:45:55 +0800

    add example datas v2.0.0.sql

---
+++ datas/v2.0.0.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_name` varchar(64) DEFAULT '' COMMENT 'user name;', -- @rename from=name
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX idx_name (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
